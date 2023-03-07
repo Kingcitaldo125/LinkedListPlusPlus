0d581c0c7a6e0675f11a05e845f0d557b369aacb
Paul Arelt <paularelt@gmail.com>
2023-03-07 17:04:46 -0500

    Refactor main; fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include "LList.hpp"
 
-using llist_plus_plus::LList;
+using llist_plus_plus::List;
 
 inline void app()
 {
-  LList mlist;
+  List mlist;
+
   mlist.add_node(3);
   mlist.add_node(2);
   mlist.add_node(1);
+
   mlist.traverse();
 }
 
